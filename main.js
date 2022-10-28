324f25dee74dded2a89dad68d6c9871b2c0f6769
lanz1311 <rvga1311@gmail.com>
2022-10-27 22:15:47 -0600

    Format Code

--- main.js
+++ main.js
@@ -3,48 +3,44 @@ function setup() {
   //Convirtiendo memoria inicial en un diccionario lógico
   mapearMemoriaTotal();
   console.log("Memoria Total ", memoriaTotal);
-  console.log("Lista De Accesos ",listaDeAccesos);
+  console.log("Lista De Accesos ", listaDeAccesos);
 }
 
 function draw() {
-  
-   let procesoSolicitado
-  let punteroActual=listaDeAccesos[0];
-   procesoSolicitado = procesoDePuntero(punteroActual);
-  if(verificarSiProcesoEnTabla(parseInt(procesoSolicitado))===false){
-            tablaDeProcesos.push({idProceso:parseInt(procesoSolicitado), accesoCompletado: false, tiempoDeVida:10, R:generateRandomIntegerRGB() , G: generateRandomIntegerRGB() , B: generateRandomIntegerRGB() })
-          }
+  let procesoSolicitado;
+  let punteroActual = listaDeAccesos[0];
+  procesoSolicitado = procesoDePuntero(punteroActual);
+  if (verificarSiProcesoEnTabla(parseInt(procesoSolicitado)) === false) {
+    tablaDeProcesos.push({
+      idProceso: parseInt(procesoSolicitado),
+      accesoCompletado: false,
+      tiempoDeVida: 10,
+      R: generateRandomIntegerRGB(),
+      G: generateRandomIntegerRGB(),
+      B: generateRandomIntegerRGB(),
+    });
+  }
   background(255);
   dibujarCronometro();
-  for (let element of memorias){
+  for (let element of memorias) {
     element.dibujarMemoria();
   }
-  if (frameCount % 60 == 0 ) {
-    if(listaDeAccesos.length>0){
-
-        for (let element of memorias){
-          element.solicitarInstruccion(punteroActual, procesoSolicitado);
+  if (frameCount % 60 == 0) {
+    if (listaDeAccesos.length > 0) {
+      for (let element of memorias) {
+        element.solicitarInstruccion(punteroActual, procesoSolicitado);
       }
-        listaDeAccesos=listaDeAccesos.slice(1);
-        if(verificarUltimoAccesoAMemoria(parseInt(procesoSolicitado))===-1){
-          for(let element in tablaDeProcesos){
-            if(tablaDeProcesos[element].idProceso===parseInt(procesoSolicitado)){
-              tablaDeProcesos[element].accesoCompletado=true;
-            }
+      listaDeAccesos = listaDeAccesos.slice(1);
+      if (verificarUltimoAccesoAMemoria(parseInt(procesoSolicitado)) === -1) {
+        for (let element in tablaDeProcesos) {
+          if (
+            tablaDeProcesos[element].idProceso === parseInt(procesoSolicitado)
+          ) {
+            tablaDeProcesos[element].accesoCompletado = true;
           }
         }
-      
+      }
     }
-   reducirTiempoProcesos();
-      
-  
-    
+    reducirTiempoProcesos();
   }
-  
-  
-  
-  
-  
-   
 }
-
--- memoriaOptima.js
+++ memoriaOptima.js
@@ -1,93 +1,112 @@
 class claseMemoriaOptima {
   constructor(tipo) {
-    this.tipo=tipo;
-    this.MMU=[];
-    this.RAM=this.memoriaDisponible=Array(100).fill(0);
-    this.memoriaAsignada=[];
-    this.cantidadDeFallosDePagina=0;
+    this.tipo = tipo;
+    this.MMU = [];
+    this.RAM = this.memoriaDisponible = Array(100).fill(0);
+    this.memoriaAsignada = [];
+    this.cantidadDeFallosDePagina = 0;
   }
-  
-  
-  dibujarMemoria(){
-  let RGB=[];
-  let posiciony=0;
 
-  this.tipo==="Óptimo"?posiciony=50:posiciony=100;
-  fill(0);
-  textSize(15);
-  text("Fallos de Página: "+this.cantidadDeFallosDePagina, (width/2)-200, posiciony-5);
-  fill(0);
-  textSize(15);
-  text( "Algoritmo "+this.tipo, (width/2)-400, posiciony-5);
-  for(let i = 0; i<100;i++){
-    
-    this.RAM[i]===0?noFill():fill(obtenerRGB(this.RAM[i])[0],obtenerRGB(this.RAM[i])[1],obtenerRGB(this.RAM[i])[2]);
-    rect(((width/2)-500)+(i*10),posiciony,10,25);
+  dibujarMemoria() {
+    let RGB = [];
+    let posiciony = 0;
+
+    this.tipo === "Óptimo" ? (posiciony = 50) : (posiciony = 100);
+    fill(0);
+    textSize(15);
+    text(
+      "Fallos de Página: " + this.cantidadDeFallosDePagina,
+      width / 2 - 200,
+      posiciony - 5
+    );
+    fill(0);
+    textSize(15);
+    text("Algoritmo " + this.tipo, width / 2 - 400, posiciony - 5);
+    for (let i = 0; i < 100; i++) {
+      this.RAM[i] === 0
+        ? noFill()
+        : fill(
+            obtenerRGB(this.RAM[i])[0],
+            obtenerRGB(this.RAM[i])[1],
+            obtenerRGB(this.RAM[i])[2]
+          );
+      rect(width / 2 - 500 + i * 10, posiciony, 10, 25);
     }
   }
-  
-  
-  
-  
 
-  solicitarInstruccion(puntero, procesoID){
-    let flagEnMemoriaAsignada=false;
-    let flagEnMMU=false;
-    let direccionMemoria=-1;
-    let tamano=0;
-    tamano=buscarTamanioPuntero(puntero);
-    tamano = Math.ceil(parseFloat(tamano)/4096);
+  solicitarInstruccion(puntero, procesoID) {
+    let flagEnMemoriaAsignada = false;
+    let flagEnMMU = false;
+    let direccionMemoria = -1;
+    let tamano = 0;
+    tamano = buscarTamanioPuntero(puntero);
+    tamano = Math.ceil(parseFloat(tamano) / 4096);
     //Se revisa si tiene memoria asignada ese puntero
-    if(this.memoriaAsignada.length===0){
-      direccionMemoria=this.MMU.length+1
-      this.memoriaAsignada.push({puntero:puntero, direccionDeMemoria:direccionMemoria+"-"+puntero})
-      flagEnMemoriaAsignada=true;
-    }else{
-      for (let element in this.memoriaAsignada){
-        if (this.memoriaAsignada[element].puntero===puntero){
-          flagEnMemoriaAsignada=true;
+    if (this.memoriaAsignada.length === 0) {
+      direccionMemoria = this.MMU.length + 1;
+      this.memoriaAsignada.push({
+        puntero: puntero,
+        direccionDeMemoria: direccionMemoria + "-" + puntero,
+      });
+      flagEnMemoriaAsignada = true;
+    } else {
+      for (let element in this.memoriaAsignada) {
+        if (this.memoriaAsignada[element].puntero === puntero) {
+          flagEnMemoriaAsignada = true;
           break;
         }
       }
     }
-    if(!flagEnMemoriaAsignada){
-      direccionMemoria=this.MMU.length+1
-      this.memoriaAsignada.push({puntero:puntero, direccionDeMemoria:direccionMemoria+"-"+puntero})
+    if (!flagEnMemoriaAsignada) {
+      direccionMemoria = this.MMU.length + 1;
+      this.memoriaAsignada.push({
+        puntero: puntero,
+        direccionDeMemoria: direccionMemoria + "-" + puntero,
+      });
     }
     //Siempre va a resultar con memoria asignada, si ya tenía no se le asigna más, si no tenía se le asigna nueva. Ahora se chequea el mapeo de las paginas virtuales usando su tabla de símbolos.
-    
-    if(this.MMU.length===0){
-      this.MMU.push({id:puntero, paginas:[], tiempoDeVida:10})
-      flagEnMMU=true;
-      for (let i=0; i<tamano; i++){
-        this.MMU[this.MMU.length-1].paginas.push({espacioEnMemoria:-1, marcado:false})
+
+    if (this.MMU.length === 0) {
+      this.MMU.push({ id: puntero, paginas: [], tiempoDeVida: 10 });
+      flagEnMMU = true;
+      for (let i = 0; i < tamano; i++) {
+        this.MMU[this.MMU.length - 1].paginas.push({
+          espacioEnMemoria: -1,
+          marcado: false,
+        });
       }
-    }else{
-      for (let element in this.MMU){
-        if (this.MMU[element].id===puntero){
-          flagEnMMU=true;
+    } else {
+      for (let element in this.MMU) {
+        if (this.MMU[element].id === puntero) {
+          flagEnMMU = true;
         }
       }
     }
-    if(!flagEnMMU){
-      this.MMU.push({id:puntero, paginas:[]});
-      for (let i=0; i<tamano; i++){
-        this.MMU[this.MMU.length-1].paginas.push({espacioEnMemoria:-1, marcado:false})
+    if (!flagEnMMU) {
+      this.MMU.push({ id: puntero, paginas: [] });
+      for (let i = 0; i < tamano; i++) {
+        this.MMU[this.MMU.length - 1].paginas.push({
+          espacioEnMemoria: -1,
+          marcado: false,
+        });
       }
     }
-    
+
     // Ahora se revisa si TODAS LAS PAGINAS DE LA TABLA DE SIMBOLOS TAMBIÉN ESTAN EN LA MEMORIA FISICA y AQUí SE PRODUCIRÍAN LOS FALLOS DE PÁGINA.
-    for (let i=0; i<this.MMU.length;i++){
-      if(this.MMU[i].id===puntero){     
-        for (let j=0; j<this.MMU[i].paginas.length;j++){
-          if(this.MMU[i].paginas[j].espacioEnMemoria === -1){
-            if(this.RAM.indexOf(0)!==-1){
+    for (let i = 0; i < this.MMU.length; i++) {
+      if (this.MMU[i].id === puntero) {
+        for (let j = 0; j < this.MMU[i].paginas.length; j++) {
+          if (this.MMU[i].paginas[j].espacioEnMemoria === -1) {
+            if (this.RAM.indexOf(0) !== -1) {
               this.MMU[i].paginas[j].espacioEnMemoria = this.RAM.indexOf(0);
-              this.RAM[this.RAM.indexOf(0)] = parseInt(procesoID)
+              this.RAM[this.RAM.indexOf(0)] = parseInt(procesoID);
               this.cantidadDeFallosDePagina++;
-            }else{
-              let indiceDeCambio = this.paginarMemoria(parseInt(procesoID), puntero);
-              this.MMU[i].paginas[j].espacioEnMemoria = indiceDeCambio
+            } else {
+              let indiceDeCambio = this.paginarMemoria(
+                parseInt(procesoID),
+                puntero
+              );
+              this.MMU[i].paginas[j].espacioEnMemoria = indiceDeCambio;
               this.cantidadDeFallosDePagina++;
             }
           }
@@ -96,100 +115,76 @@ class claseMemoriaOptima {
       }
     }
     console.log("Memoria Asignada", this.memoriaAsignada);
-    console.log("MMU ",this.MMU);
-    console.log("RAM ",this.RAM);
+    console.log("MMU ", this.MMU);
+    console.log("RAM ", this.RAM);
     console.log("tabla de proceso", tablaDeProcesos);
-    console.log("-------------------------------------------------")
+    console.log("-------------------------------------------------");
   }
-  
-  paginarMemoria(nuevoProceso, nuevoPuntero){
+
+  paginarMemoria(nuevoProceso, nuevoPuntero) {
     let arregloOrdenado;
-    let punteroEscogido
-    let indiceDeCambio=0;
-    for(let element in tablaDeProcesos){
-      if(tablaDeProcesos[element].idProceso!==nuevoProceso){
-        if(verificarUltimoAccesoAMemoria(tablaDeProcesos[element].idProceso)===-1){
-          indiceDeCambio = this.RAM.indexOf(tablaDeProcesos[element].idProceso)
-          this.RAM[indiceDeCambio]=nuevoProceso;
-          for (let element1 in this.MMU){
-            for(let element2 in this.MMU[element1].paginas){
-              if(this.MMU[element1].paginas[element2].espacioEnMemoria===indiceDeCambio){
-                this.MMU[element1].paginas[element2].espacioEnMemoria=-1;
+    let punteroEscogido;
+    let indiceDeCambio = 0;
+    for (let element in tablaDeProcesos) {
+      if (tablaDeProcesos[element].idProceso !== nuevoProceso) {
+        if (
+          verificarUltimoAccesoAMemoria(tablaDeProcesos[element].idProceso) ===
+          -1
+        ) {
+          indiceDeCambio = this.RAM.indexOf(tablaDeProcesos[element].idProceso);
+          this.RAM[indiceDeCambio] = nuevoProceso;
+          for (let element1 in this.MMU) {
+            for (let element2 in this.MMU[element1].paginas) {
+              if (
+                this.MMU[element1].paginas[element2].espacioEnMemoria ===
+                indiceDeCambio
+              ) {
+                this.MMU[element1].paginas[element2].espacioEnMemoria = -1;
                 return indiceDeCambio;
               }
             }
           }
-            
         }
       }
     }
-    arregloOrdenado=llamadaMasTardia(nuevoPuntero);
-    for(let i = arregloOrdenado.length-1; i>=0; i--){
-        punteroEscogido=arregloOrdenado[i];
-        for(let x in this.MMU){
-          if(this.MMU[x].id===punteroEscogido){
-            for(let y in this.MMU[x].paginas){
-              if(this.MMU[x].paginas[y].espacioEnMemoria!==-1){
-                indiceDeCambio=this.MMU[x].paginas[y].espacioEnMemoria;
-                this.MMU[x].paginas[y].espacioEnMemoria=-1;
-                this.RAM[indiceDeCambio] = nuevoProceso;
-                return indiceDeCambio;
-              }
+    arregloOrdenado = llamadaMasTardia(nuevoPuntero);
+    for (let i = arregloOrdenado.length - 1; i >= 0; i--) {
+      punteroEscogido = arregloOrdenado[i];
+      for (let x in this.MMU) {
+        if (this.MMU[x].id === punteroEscogido) {
+          for (let y in this.MMU[x].paginas) {
+            if (this.MMU[x].paginas[y].espacioEnMemoria !== -1) {
+              indiceDeCambio = this.MMU[x].paginas[y].espacioEnMemoria;
+              this.MMU[x].paginas[y].espacioEnMemoria = -1;
+              this.RAM[indiceDeCambio] = nuevoProceso;
+              return indiceDeCambio;
             }
           }
         }
-      
-      
+      }
     }
   }
-  
-  eliminaProcesoDeMemoria(idProceso){
- 
+
+  eliminaProcesoDeMemoria(idProceso) {
     for (let index = 0; index < this.RAM.length; index++) {
       if (this.RAM[index] === idProceso) {
         this.RAM[index] = 0;
       }
     }
   }
-  
 
-  
-  
-  
-  
-  eliminarDeMMUyMemoriaAsignada(proceso){
+  eliminarDeMMUyMemoriaAsignada(proceso) {
     for (let i = this.MMU.length - 1; i >= 0; --i) {
-        if (parseInt(procesoDePuntero(this.MMU[i].id)) === proceso) {
-            this.MMU.splice(i,1);
-         
-        }
+      if (parseInt(procesoDePuntero(this.MMU[i].id)) === proceso) {
+        this.MMU.splice(i, 1);
+      }
     }
-     for (let i = this.memoriaAsignada.length - 1; i >= 0; --i) {
-        if (parseInt(procesoDePuntero(this.memoriaAsignada[i].puntero)) === proceso) {
-            this.memoriaAsignada.splice(i,1);
-          
-        }
+    for (let i = this.memoriaAsignada.length - 1; i >= 0; --i) {
+      if (
+        parseInt(procesoDePuntero(this.memoriaAsignada[i].puntero)) === proceso
+      ) {
+        this.memoriaAsignada.splice(i, 1);
+      }
     }
-    
-    
   }
-  
- 
 }
-
-
-
-
- 
-
- 
-
-
-
-
-
-
-
-
-
-
